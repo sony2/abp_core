8411a0a868fcbd7fb08dbbf27a0d346909f3757f
oscalarr <oscar.larrayoz@helvetia.es>
2023-05-06 11:01:34 +0200

    Add belen content

---
+++ src/Plugin/Block/BelenDetailsBlock.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\abp_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+
+/**
+ * Provides an evbelenent details block.
+ *
+ * @Block(
+ *   id = "abp_core_event_details",
+ *   admin_label = @Translation("Belen Details"),
+ *   category = @Translation("ABP")
+ * )
+ */
+class BelenDetailsBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = \Drupal::routeMatch()->getParameter('node');
+
+    if ($node->getType() != 'event') {
+      return;
+    }
+
+    $time = $formatted_date = $link = '';
+
+    // Format the date and time.
+    if (!empty($node->get('field_date')->value)) {
+      $date_value = $node->get('field_date')->value;
+      $date = date_create($date_value);
+      $formatted_date = date_format($date, "d F Y");
+      $time = date_format($date, "h:i A");
+    }
+
+    if (!empty($node->get('field_link')->uri)) {
+      $uri = $node->get('field_link')->uri;
+      $link = Url::fromUri($uri)->toString();
+    }
+
+    $build = [
+      '#theme' => 'abp_core_belen_details',
+      '#date' => $formatted_date,
+      '#time' => $time,
+      '#location' => $node->get('field_location')->value,
+      '#link' => $link,
+    ];
+
+    $build['#cache']['max-age'] = 0;
+
+    return $build;
+  }
+
+}
